d7e9935c79962f2dbb48a0ee7655f9028d47f9b1
Przemysław Fikas <81600247+fikasp@users.noreply.github.com>
2024-03-01 22:00:09 +0100

    Update Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,16 +5,16 @@ export default function Header() {
 	return (
 		<header className="sticky top-0 flex-center w-full backdrop-blur-md bg-black/50 shadow-xl text-white p-4">
 			<div className="container flex-between">
-				<div>
-					<h1 className="text-2xl font-bold w-[100px]">
+				<div className="flex w-[100px]>
+					<h1 className="text-2xl font-bold">
 						<Link href={`/`}>ARW</Link>
 					</h1>
 				</div>
 				<div>
 					<h1 className="font-bold">Menu</h1>
 				</div>
-				<div>
-					<h1 className="flex-end w-[100px]">
+				<div className="flex-end w-[100px]">
+					<h1>
 						<UserButton afterSignOutUrl="/" />
 					</h1>
 				</div>
