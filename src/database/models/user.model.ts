7a0551da57a96f2602060a1e30a30604171604a0
fikasp <fikasp@gmail.com>
2024-04-04 18:48:23 +0200

    Add Clerk Webhook

---
+++ src/database/models/user.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model, models } from 'mongoose'
+
+const UserSchema = new Schema({
+	clerkId: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	photo: {
+		type: String,
+		required: true,
+	},
+	firstName: {
+		type: String,
+	},
+	lastName: {
+		type: String,
+	},
+})
+
+const User = models?.User || model('User', UserSchema)
+
+export default User
