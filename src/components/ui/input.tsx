054e00e07f441293873e189fafde9abef86aa049
fikasp <fikasp@gmail.com>
2024-05-11 05:15:03 +0200

    Fix colors

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 			<input
 				type={type}
 				className={cn(
-					'flex h-10 w-full rounded-md px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-base-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-base-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:ring-offset-base-950 dark:placeholder:text-base-600 dark:focus-visible:ring-base-300 bg-base-50 dark:bg-base-950/50 ',
+					'flex h-10 w-full rounded-md px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-base-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-base-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:ring-offset-base-950 dark:placeholder:text-base-500 dark:focus-visible:ring-base-300 bg-base-50 dark:bg-base-800 ',
 					className
 				)}
 				ref={ref}
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
 		return (
 			<textarea
 				className={cn(
-					'flex min-h-[80px] w-full rounded-md px-3 py-2 text-sm ring-offset-white placeholder:text-base-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-base-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:ring-offset-base-950 dark:placeholder:text-base-600 dark:focus-visible:ring-base-300 bg-base-50 dark:bg-base-950/50',
+					'flex min-h-[80px] w-full rounded-md px-3 py-2 text-sm ring-offset-white placeholder:text-base-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-base-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:ring-offset-base-950 dark:placeholder:text-base-500 dark:focus-visible:ring-base-300 bg-base-50 dark:bg-base-800',
 					className
 				)}
 				ref={ref}
