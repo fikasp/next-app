a53695947e9b39905647649abfc5381120a5a751
fikasp <fikasp@gmail.com>
2024-05-25 06:38:11 +0200

    Fix apos

--- src/app/(root)/error.tsx
+++ src/app/(root)/error.tsx
@@ -8,7 +8,7 @@ export default function Error() {
 		<ArwContainer center className="gap-6">
 			<ArwTitle>Oops! Something went wrong...</ArwTitle>
 			<ArwText center>
-				It seems that we've encountered an unexpected issue while using the	application. 
+				It seems that we&apos;ve encountered an unexpected issue while using the application. 
 			</ArwText>
       <ArwText center>
 				We apologize for any inconvenience this may have caused.
