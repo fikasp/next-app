9d44bcfe42c37163dd5374e7c9dc18f3e638d031
fikasp <fikasp@gmail.com>
2024-05-25 06:33:46 +0200

    Add error page

---
+++ src/app/(root)/error.tsx
@@ -0,0 +1,18 @@
+'use client'
+import ArwContainer from '@/components/arw/ArwContainer'
+import ArwText from '@/components/arw/ArwText'
+import ArwTitle from '@/components/arw/ArwTitle'
+
+export default function Error() {
+	return (
+		<ArwContainer center className="gap-6">
+			<ArwTitle>Oops! Something went wrong...</ArwTitle>
+			<ArwText center>
+				It seems that we've encountered an unexpected issue while using the	application. 
+			</ArwText>
+      <ArwText center>
+				We apologize for any inconvenience this may have caused.
+			</ArwText>
+		</ArwContainer>
+	)
+}
