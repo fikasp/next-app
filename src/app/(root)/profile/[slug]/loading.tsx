237b81b91e8d0c208aa88deb89b126bbc64335d6
fikasp <fikasp@gmail.com>
2024-07-04 17:02:53 +0200

    Refactor: Restore project loading

---
+++ src/app/(root)/profile/[slug]/loading.tsx
@@ -0,0 +1,7 @@
+import ProjectLoading from '@/components/pages/ProjectLoading'
+import { debug } from '@/lib/utils/dev'
+
+export default function Loading() {
+	debug(4)
+	return <ProjectLoading />
+}
