f53599192c77d893417249636e9005ea4b845671
fikasp <fikasp@gmail.com>
2024-08-24 04:46:43 +0200

    Fix: Upload image

--- src/lib/handlers/project.handlers.ts
+++ src/lib/handlers/project.handlers.ts
@@ -67,7 +67,7 @@ export const handleAddImageToProject = async (
 	debug(2, 9, formData)
 	try {
 		const uploadedImage: UploadedImage = await uploadImage(formData)
-		if (uploadedImage.name) {
+		if (!uploadedImage.name) {
 			toastError('Image failed to upload.')
 			return
 		}
