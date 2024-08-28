fddb4448cee3c76c66a05b88f935cc1b787dcb84
fikasp <fikasp@gmail.com>
2024-08-28 15:55:11 +0200

    Config: Project metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ const roboto = Roboto({
 
 // metadata
 export const metadata: Metadata = {
-	title: 'ARW Next App Template',
-	description: 'Projects CRUD',
+	title: 'ARW Next App',
+	description: 'Next.js template with MongoDB and Cloudinary',
 }
 
 export default function RootLayout({
