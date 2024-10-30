8a468440ed129b95811d3600ce3420d1b3a85d43
fikasp <fikasp@gmail.com>
2024-10-30 16:52:44 +0100

    Remove: Scroll-m-4

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -4,7 +4,7 @@ import Main from '@/components/layout/Main'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="flex flex-col justify-between min-h-screen scroll-m-4">
+		<div className="flex flex-col justify-between min-h-screen">
 			<Header />
 			<Main>{children}</Main>
 			<Footer />
